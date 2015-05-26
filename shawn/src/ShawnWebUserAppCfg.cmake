5e7f76cacb81c7e1a5655242df7733c79eb079b1
HernanBareiro <hernanbareiro@gmail.com>
2015-05-26 11:11:56 -0300

    simple_app

---
+++ shawn/src/ShawnWebUserAppCfg.cmake
@@ -0,0 +1,16 @@
+#=============
+# ShawnWeb User application configuration
+#
+# Allow configure the user application that will be compiled and executed
+#=============
+
+#=============
+# Set variables of the user
+# This variables will be set through the Web scritp that andle files
+#=============
+
+#Set a variable with the user id
+set( USER_ID usr_1)
+
+#Set a variable with the module name of the user
+set(USER_MODULE_NAME proyecto_testing12)
